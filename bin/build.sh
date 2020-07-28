fbaa81cdf94a2fadb36b2aa865e682907e5887f4
Chiaki Obara <obara@ham.works>
2020-07-28 20:21:19 +0900

    file delete, move and rewrite.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+set -ex
+
+if [ $# -lt 2 ]; then
+	echo "usage: $0 <plugin-name> <version>"
+	exit 1
+fi
+
+pluginname=$1
+version=$2
+
+if [ ! `echo $version | grep -e 'alpha' -e 'beta' -e 'RC' -e 'rc'` ] ; then
+  sed -i.bak -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.txt;
+fi
+rm readme.txt.bak
+
+sed -i.bak -e "s/^ \* Version: .*/ * Version: ${version}/g" ${pluginname}.php;
+sed -i.bak -e "s/^ \* @version .*/ * @version ${version}/g" ${pluginname}.php;
+
+rm ${pluginname}.php.bak
 npm install
 npm run build
-sed -i -e "s/nightly/${TRAVIS_TAG}/" $(basename $TRAVIS_REPO_SLUG).php
-sed -i -e "s/nightly/${TRAVIS_TAG}/" readme.txt
+
+rsync -a --exclude-from=.distignore ./ ./nightly/
+
+rm -rf build
+
