993996e111ac78c9109ee01c7d7e6724562f07ed
Chiaki Obara <obara@ham.works>
2023-09-27 14:41:38 +0900

    fix: init

--- simple-definition-list-blocks.php
+++ simple-definition-list-blocks.php
@@ -17,58 +17,19 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Load text domain.
  */
-function simple_definition_list_blocks_load_textdomain() {
+add_action( 'init', function () {
 	load_plugin_textdomain( 'simple-definition-list-blocks', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-}
-add_action( 'init', 'simple_definition_list_blocks_load_textdomain' );
+} );
 
 /**
  * Block registration.
  */
 function simple_definition_list_blocks_register_block() {
-	$asset_file = include plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
-	wp_register_script(
-		'simple-definition-list-blocks',
-		plugins_url( 'build/index.js', __FILE__ ),
-		$asset_file['dependencies'],
-		$asset_file['version'],
-		true
-	);
-
-	register_block_type(
-		'simple-definition-list-blocks/list',
-		array(
-			'editor_script' => 'simple-definition-list-blocks',
-		)
-	);
-
-	register_block_type(
-		'simple-definition-list-blocks/term',
-		array(
-			'editor_script' => 'simple-definition-list-blocks',
-		)
-	);
-
-	register_block_type(
-		'simple-definition-list-blocks/details',
-		array(
-			'editor_script' => 'simple-definition-list-blocks',
-		)
-	);
-
-	register_block_type(
-		'simple-definition-list-blocks/details-html',
-		array(
-			'editor_script' => 'simple-definition-list-blocks',
-		)
-	);
-
-	register_block_type(
-		'simple-definition-list-blocks/div',
-		array(
-			'editor_script' => 'simple-definition-list-blocks',
-		)
-	);
+	register_block_type( __DIR__ . '/build/blocks/definition-details' );
+	register_block_type( __DIR__ . '/build/blocks/definition-details-html' );
+	register_block_type( __DIR__ . '/build/blocks/definition-div' );
+	register_block_type( __DIR__ . '/build/blocks/definition-list' );
+	register_block_type( __DIR__ . '/build/blocks/definition-term' );
 }
 
 add_action( 'init', 'simple_definition_list_blocks_register_block' );
