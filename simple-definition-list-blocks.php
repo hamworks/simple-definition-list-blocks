fd040f7bcd758c06c8b37f9d653914e6fae1a57c
Hiroshi Urabe <urabe@ham.works>
2024-03-12 19:56:48 +0900

    Release 6.0.1

--- simple-definition-list-blocks.php
+++ simple-definition-list-blocks.php
@@ -6,7 +6,7 @@
  * Author:            mel_cha
  * Author URI:        https://chiilog.com
  * Text Domain:       simple-definition-list-blocks
- * Version: 6.0.0
+ * Version: 6.0.1
  * Requires at least: 5.8
  *
  * @package         Simple_Definition_List_Blocks
